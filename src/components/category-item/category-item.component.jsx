30e760696e779f3eeb5cb12aa53b5d28c8e915cd
jtiser <jtiser@acolad.com>
2022-05-27 11:27:49 +0200

    styling

--- src/components/category-item/category-item.component.jsx
+++ src/components/category-item/category-item.component.jsx
@@ -10,7 +10,7 @@ const CategoryItem = ({ category }) => {
       />
       <div className="category-body-container">
         <h2>{title}</h2>
-        <p>Shop now</p>
+        <p>Shop Now</p>
       </div>
     </div>
   );
